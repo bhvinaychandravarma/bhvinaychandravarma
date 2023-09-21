f7ce7f1926c88260890708c0e2d37ea0fb1d78cc
bhvinaychandravarma <65181569+bhvinaychandravarma@users.noreply.github.com>
2023-09-21 14:21:51 -0400

    Update VRIT.py

--- VRIT.py
+++ VRIT.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+headerSection = st.container()
+mainSection = st.container()
+leftNav = st.sidebar
+
+with headerSection:
+    st.title("Streamlit")
+    st.markdown("This is a demo of Streamlit")
